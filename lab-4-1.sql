713d466d410e810716a6a145ab3df6f6f02f2252
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-04-08 21:18:14 +0000

    More Class Stuff

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,9 @@
 -- Expected result:
 -- 2935
 
-
+Select Sum(stats.hits)
+From stats
+Inner Join players 
+On stats.player_id = players.id 
+Where players.first_name = "Barry" 
+And players.last_name = "Bonds";
